8cf8aaaddb0a889bb2692f0b19cfb2b3653792e9
zhuna <zn@tedu.cn>
2019-08-21 15:07:51 +0800

    添加首页图片

--- src/views/toPay.vue
+++ src/views/toPay.vue
@@ -2,6 +2,7 @@
  <template>
  <div class="container">
      <top-header></top-header>
+     <!-- 顶部 -->
      <div class="top">
          <div class="logo">
            <img src="../../public/imgs/logo.jpg" alt="">
@@ -10,9 +11,24 @@
            <h3 class="loginText">结算</h3>
         </div>
      </div>
+     <!-- 付款主体 -->
+     <div class="pay_body">
+         <div class="check_item">
+             <h4>填写核对订单信息</h4>
+         </div>
+         <hr>
+     </div>
  </div>
  </template>
  <style scoped>
+ .check_item{
+   width:25%;
+   height:5%;
+   background:rgb(8, 153, 69); 
+   color:#fff;
+   padding:0.5rem;
+   border-radius:8%; 
+}
  .top{
     width:100%;
     height:5rem;
@@ -30,4 +46,5 @@
     margin-left:10rem;
     margin-top:1rem;
 }
+
  </style>
